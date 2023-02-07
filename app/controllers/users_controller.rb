9b56eca12ea7bfeb495acc3f8bdf68389f4c400f
sandeep saini <sandeepsaini14u@gmail.com>
2023-02-07 12:32:41 +0530

    adding_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,36 @@
 class UsersController < ApplicationController
-#   def index
-#     @users = User.all
-#   end
+  def index
+    @users = User.all
+  end
 
-#   def show
-    
-#   end
+  def show
+    @user = User.find(params[:id])
+  end
 
-#   def new
-#     @user = User.new
-#   end
+  def new
+    @user = User.new
+  end
 
-#   def create
-#     @user = User.new(user_params)
-#     if @user.save
-#       redirect_to @user
-#     end 
-#   end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new , status: :unprocessable_entity
+      end 
+    end
 
-#   def edit
+  def edit
 
-#   end
+  end
+ 
 
-#   private
-#   def user_params
-#     params.require(:user).permit(:name, :)
+
+private
+
+def user_params
+  params.require(:user).permit(:first_name, :last_name, :gender, :contact_no, :age, 
+    :occupation, :address, :city, :pin_code, :state, :dob, :marital_status)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'users#_form'
   get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
+  root 'users#index'
+  resources :users
+   
 end
